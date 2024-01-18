3c7c18eb700adfc028e387559a95dfc82680c6bf
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2024-01-18 13:16:51 +0100

    Revert "Added and tested the ability to display  messages"
    This reverts commit 4d3e977134d36bd458c7bdf1a94a19feae51e3ff.

--- src/app/chatpage/chatpage.component.ts
+++ src/app/chatpage/chatpage.component.ts
@@ -1,27 +1,12 @@
-import {Component, inject} from '@angular/core';
-import {collection, Firestore, getDocs, query} from "@angular/fire/firestore";
-import {NgFor} from "@angular/common";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chatpage',
   standalone: true,
-  imports: [
-    NgFor
-  ],
+  imports: [],
   templateUrl: './chatpage.component.html',
   styleUrl: './chatpage.component.css'
 })
 export class ChatpageComponent {
-  firestore = inject(Firestore);
-  messages: any = [];
 
-  async ngOnInit() {
-    const q = query(collection(this.firestore, "FirestoreTest"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      const message = doc.data();
-      console.log(message);
-      this.messages.push(message)
-    })
-  }
 }
